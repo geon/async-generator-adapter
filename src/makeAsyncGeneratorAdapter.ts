41e7beaf4bfba808c1436790a27c696c2aa116f6
Victor Widell <victor.widell@divid.se>
2019-02-07 15:38:30 +0100

    Removed the terminator generator.

--- src/makeAsyncGeneratorAdapter.ts
+++ src/makeAsyncGeneratorAdapter.ts
@@ -20,55 +20,34 @@ function makePromiseAndCallbacks<T>(): PromiseAndCallbacks<T> {
 	};
 }
 
+interface AsyncTerminator<T> {
+	next: (data: T) => Promise<void>;
+	throw: (error: Error) => void;
+}
+
 export function makeAsyncGeneratorAdapter<T>(
-	job: (iterator: {
-		next: (data: T) => Promise<IteratorResult<undefined>>;
-		throw: (error: Error) => Promise<IteratorResult<undefined>>;
-	}) => Promise<void>,
+	job: (iterator: AsyncTerminator<T>) => Promise<void>,
 ): AsyncIterableIterator<T> {
-	let newData: PromiseAndCallbacks<T>;
-
-	function setNewDataPromise() {
-		newData = makePromiseAndCallbacks<T>();
-	}
-	setNewDataPromise();
-
-	let handled: PromiseAndCallbacks<void>;
-
-	function setHandledPromise() {
-		handled = makePromiseAndCallbacks<void>();
-	}
-	setHandledPromise();
+	let newData = makePromiseAndCallbacks<T>();
+	let handled = makePromiseAndCallbacks<void>();
 
 	const done = makePromiseAndCallbacks<Symbol>();
 	const doneSymbol = Symbol();
 
-	const asyncTerminator = (async function*() {
-		for (;;) {
-			try {
-				const data: T = yield;
-				newData!.resolve(data);
-			} catch (error) {
-				newData!.reject(error);
-				return;
-			}
-
-			await handled!.promise;
-			setHandledPromise();
-		}
-	})();
-
-	// Prime it, pausing at the yield.
-	asyncTerminator.next();
-
-	job({ next: asyncTerminator.next, throw: asyncTerminator.throw! }).then(() =>
-		done.resolve(doneSymbol),
-	);
+	job({
+		next: (data: T) => {
+			newData.resolve(data);
+			return handled.promise;
+		},
+		throw: (error: Error) => {
+			newData.reject(error);
+		},
+	}).then(() => done.resolve(doneSymbol));
 
 	return (async function* asyncGenerator() {
 		for (;;) {
-			const data = await Promise.race([done.promise, newData!.promise]);
-			setNewDataPromise();
+			const data = await Promise.race([done.promise, newData.promise]);
+			newData = makePromiseAndCallbacks<T>();
 
 			if (data === doneSymbol) {
 				return;
@@ -76,9 +55,10 @@ export function makeAsyncGeneratorAdapter<T>(
 
 			try {
 				yield data as T;
-				handled!.resolve();
+				handled.resolve();
+				handled = makePromiseAndCallbacks<void>();
 			} catch (error) {
-				handled!.reject(error);
+				handled.reject(error);
 				return;
 			}
 		}
